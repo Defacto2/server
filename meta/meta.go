6bd32559f6050cdac5f1bf875df591ed03347c16
Ben Garrett <code.by.ben@gmail.com>
2023-01-12 06:53:24 +1100

    finished categories metadata.

--- meta/meta.go
+++ meta/meta.go
@@ -12,15 +12,6 @@ import (
 
 type Tag int
 
-const (
-	Announcement Tag = iota
-	ANSIEditor
-	AppleII
-	AtariST
-	BBS
-	Logo
-)
-
 type URI map[Tag]string
 
 type Name map[Tag]string
@@ -29,6 +20,34 @@ type Info map[Tag]string
 
 type Count map[Tag]int
 
+const (
+	Announcement Tag = iota
+	ANSIEditor
+	AppleII
+	AtariST
+	BBS
+	Logo
+	Bust
+	Drama
+	Rule
+	Tool
+	Intro
+	Demo
+	ForSale
+	Ftp
+	GameHack
+	Job
+	Guide
+	Interview
+	Mag
+	News
+	Nfo
+	NfoTool
+	Proof
+	Restrict
+	Install
+)
+
 var URIs = URI{
 	Announcement: "announcements",
 	ANSIEditor:   "ansieditor",
@@ -36,6 +55,25 @@ var URIs = URI{
 	AtariST:      "atarist",
 	BBS:          "bbs",
 	Logo:         "logo",
+	Bust:         "takedown",
+	Drama:        "politics",
+	Rule:         "scenerules",
+	Tool:         "programmingtool",
+	Intro:        "releaseadvert",
+	Demo:         "demo",
+	ForSale:      "forsale",
+	Ftp:          "ftp",
+	GameHack:     "gamehack",
+	Job:          "groupapplication",
+	Guide:        "guide",
+	Interview:    "interview",
+	Mag:          "magazine",
+	News:         "newsmedia",
+	Nfo:          "releaseinformation",
+	NfoTool:      "nfotool",
+	Proof:        "releaseproof",
+	Restrict:     "internaldocument",
+	Install:      "releaseinstall",
 }
 
 var Names = URI{
@@ -45,15 +83,59 @@ var Names = URI{
 	AtariST:      "Atari ST",
 	BBS:          "BBS",
 	Logo:         "Brand art or logo",
+	Bust:         "Bust or takedown",
+	Drama:        "Community drama",
+	Rule:         "Community standard",
+	Tool:         "Computer tool",
+	Intro:        "Cracktro or intro",
+	Demo:         "Demo program",
+	ForSale:      "For sale",
+	Ftp:          "FTP",
+	GameHack:     "Game hack",
+	Job:          "Group role or job",
+	Guide:        "Guides and how-tos",
+	Interview:    "Interview",
+	Mag:          "Magazine",
+	News:         "Mainstream news",
+	Nfo:          "NFO file or scene release",
+	NfoTool:      "NFO tool",
+	Proof:        "Release proof",
+	Restrict:     "Restricted",
+	Install:      "Scene software install",
 }
 
 var Infos = Info{
-	Announcement: "Public announcements by Scene groups and organisations.",
-	ANSIEditor:   "Programs that enable you to create and edit ANSI and ASCII art.",
-	AppleII:      "Files pertaining to the Scene on the Apple II computer platform.",
-	AtariST:      "Files pertaining to the Scene on the Atari ST computer platform.",
-	BBS:          "Files pertaining to the Scene operating over telephone based BBS (Bulletin Board System) systems.",
-	Logo:         "Branding logos used by scene groups and organisations.",
+	Announcement: "Public announcements by Scene groups and organisations",
+	ANSIEditor:   "Programs that enable you to create and edit ANSI and ASCII art",
+	AppleII:      "Files pertaining to the Scene on the Apple II computer platform",
+	AtariST:      "Files pertaining to the Scene on the Atari ST computer platform",
+	BBS:          "Files pertaining to the Scene operating over telephone based BBS (Bulletin Board System) systems",
+	Logo:         "Branding logos used by scene groups and organisations",
+	Bust: "First hand accounts and third party reports on the arrest, " +
+		"bust or take-down of an active person in the scene or a scene organisation",
+	Drama: "Used for anything political that doesn't fall into the other " +
+		"categories. Usually contains documents where people, groups or " +
+		"organisations are complaining",
+	Rule: "Various codes of conduct and agreements created by scene groups and organisations",
+	Tool: "Miscellaneous tools including fixes, intro generators and BBS software",
+	Intro: "A multimedia program that is designed to promote a scene group or organisation. " +
+		"Otherwise known as a cracktro, crack intro or loader",
+	Demo:    "An artistic multimedia program that is designed to promote a demo group or collective",
+	ForSale: "Adverts for commercial physical goods and online services, varying in legality",
+	Ftp: "Files pertaining to the scene operating over Internet based " +
+		"FTP (File Transfer Protocol) servers",
+	GameHack: "Trainers, dox, cheats, and walk-throughs, which include guides, " +
+		"how-to documents and tools to complete games",
+	Job:       "Documents used by scene groups to advertise or enrol new members",
+	Guide:     "Guides and how-to documents on how to hack and crack or on the workings of the scene",
+	Interview: "Conversations conducted with scene personalities",
+	Mag:       "Reports and written articles created by scene members about the scene",
+	News:      "Mainstream media outlets reports on the scene",
+	Nfo:       "A text file or readme used to describe a scene release, group or organisation",
+	NfoTool:   "Programs that enable you to create or view NFO text files",
+	Proof:     "Evidence of the source media, usually a photo or scanned image",
+	Restrict:  "Documents created by scene groups that were often never intended to be public",
+	Install:   "A program to help an end-user install a scene release",
 }
 
 var Counts = Count{
@@ -63,6 +145,25 @@ var Counts = Count{
 	AtariST:      0,
 	BBS:          0,
 	Logo:         0,
+	Bust:         0,
+	Drama:        0,
+	Rule:         0,
+	Tool:         0,
+	Intro:        0,
+	Demo:         0,
+	ForSale:      0,
+	Ftp:          0,
+	GameHack:     0,
+	Job:          0,
+	Guide:        0,
+	Interview:    0,
+	Mag:          0,
+	News:         0,
+	Nfo:          0,
+	NfoTool:      0,
+	Proof:        0,
+	Restrict:     0,
+	Install:      0,
 }
 
 type Meta struct {
