25244f7ed543fc442de8d17897aef0cdc564a388
Ben Garrett <code.by.ben@gmail.com>
2023-01-16 16:10:07 +1100

    http loggers and template handlers.

--- config/config.go
+++ config/config.go
@@ -3,15 +3,20 @@ package config
 
 // Config options for the Defacto2 server.
 type Config struct {
+
 	// HTTPPort is the port to be used by the HTTP server.
 	HTTPPort int `env:"DEFACTO2_PORT" envDefault:"1323"`
+
 	// IsProduction reduces the console feedback.
 	IsProduction bool `env:"DEFACTO2_PRODUCTION"`
+
 	// LogRequests uses the logger middleware to save HTTP requests to a file.
 	LogRequests bool `env:"DEFACTO2_REQUESTS" envDefault:"false"`
+
 	// NoRobots enables the X-Robots-Tag noindex and nofollow HTTP header for all server request.
 	// This should never be enabled on production environments as search engines never crawl the website.
 	NoRobots bool `env:"DEFACTO2_NOROBOTS" envDefault:"false"`
+
 	// Timeout in seconds for the HTTP server.
 	Timeout uint `env:"DEFACTO2_TIMEOUT" envDefault:"5"`
 }
--- config/logger.go
+++ config/logger.go
@@ -10,91 +10,16 @@ import (
 	"github.com/Defacto2/server/logger"
 	"github.com/Defacto2/server/router/html3"
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 	"go.uber.org/zap"
 )
 
-// https://echo.labstack.com/middleware/logger/#configuration
-
-// NOTE: these current already return a 200 status code.
-// https://github.com/labstack/echo/issues/2310
-
-// LoggerDeveloper prints both requests and request errors to the console.
-var LoggerDeveloper = middleware.RequestLoggerConfig{
-	LogStatus:   true,
-	LogURI:      true,
-	LogError:    true,
-	HandleError: true, // forwards error to the global error handler, so it can decide appropriate status code
-	LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
-		if v.Error == nil {
-			fmt.Printf("REQUEST: uri: %v, status: %v\n", v.URI, v.Status)
-		} else {
-			fmt.Printf("REQUEST_ERROR: uri: %v, status: %v, err: %v\n", v.URI, v.Status, v.Error)
-		}
-		return nil
-	},
-}
-
-func LoggerProduction(log *zap.SugaredLogger) middleware.RequestLoggerConfig {
-	return middleware.RequestLoggerConfig{
-		LogURI:      true,
-		LogStatus:   true,
-		LogError:    true,
-		HandleError: true, // forwards error to the global error handler, so it can decide appropriate status code
-		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
-			if v.Error == nil {
-				log.Info("request",
-					zap.String("URI", v.URI),
-					zap.Int("status", v.Status),
-				)
-			} else {
-				log.Error("request error",
-					zap.String("URI", v.URI),
-					zap.Int("status", v.Status),
-					zap.Error(v.Error),
-				)
-			}
-			return nil
-		}}
-}
-
 // https://github.com/labstack/echo/discussions/1820
 
-var CustomMiddleware = func(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		timeStarted := time.Now()
-		err := next(c)
-
-		status := c.Response().Status
-
-		httpErr := new(echo.HTTPError)
-		if errors.As(err, &httpErr) {
-			status = httpErr.Code
-		}
-
-		fields := map[string]interface{}{
-			"latency": int64(time.Since(timeStarted) / time.Millisecond),
-			"method":  c.Request().Method,
-			"path":    c.Request().URL.Path,
-			"query":   c.Request().URL.RawQuery,
-			"status":  status,
-		}
-
-		if err != nil {
-			fmt.Printf("on error: %v\n", fields)
-			//s.logger.SendErrorWithFields(err, fields)
-			return err
-		}
-		fmt.Printf("fields: %v\n", fields)
-		//s.logger.SendWithFields(fields)
-		return nil
-	}
-}
-
-func (c Config) LoggerMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+// LoggerMiddleware handles the logging of HTTP servers.
+func (cfg Config) LoggerMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	// Logger
 	var log *zap.SugaredLogger
-	switch c.IsProduction {
+	switch cfg.IsProduction {
 	case true:
 		log = logger.Production().Sugar()
 		defer log.Sync()
@@ -117,63 +42,69 @@ func (c Config) LoggerMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 			"latency": int64(time.Since(timeStarted) / time.Millisecond),
 			"method":  c.Request().Method,
 			"path":    c.Request().URL.Path,
-			"query":   c.Request().URL.RawQuery,
-			"status":  status,
+			//"query":   c.Request().URL.RawQuery,
+			"status": status,
 		}
-
+		switch status {
+		case http.StatusOK:
+			return nil
+		}
 		if err != nil {
-			fmt.Printf("REQUEST_ERROR: uri: %v, status: %v, err: %v\n", v["path"], v["status"], err.Error())
-			return err // THIS MUST BE returned otherwise 200 will always be sent to the client
+			log.Debugf("HTTP %s %d: %s  info: %s", v["method"], v["status"], v["path"], err)
+			// This error MUST be returned otherwise the client will always receive a 200 OK status
+			return err
 		}
-		fmt.Printf("REQUEST: uri: %v, status: %v\n", v["path"], v["status"])
 		return nil
 	}
 }
 
-// TODO:
-func (x Config) CustomErrorHandler(err error, c echo.Context) {
-	//c.String(500, "oh heck! "+err.Error())
-	code := http.StatusInternalServerError
-	if he, ok := err.(*echo.HTTPError); ok {
-		code = he.Code
+// CustomErrorHandler handles customer error templates.
+func (cfg Config) CustomErrorHandler(err error, c echo.Context) {
+	var log *zap.SugaredLogger
+	switch cfg.IsProduction {
+	case true:
+		log = logger.Production().Sugar()
+		defer log.Sync()
+	default:
+		log = logger.Development().Sugar()
+		defer log.Sync()
 	}
-	//c.Logger().Error(err) // TODO: use zap or custom logger
-	errorPage := fmt.Sprintf("%d.html", code)
-	if err := c.File(errorPage); err != nil {
-		//c.Logger().Error(err)
-		fmt.Println(err)
-	}
-	splitPaths := func(r rune) bool {
-		return r == '/'
-	}
-	rel := strings.FieldsFunc(c.Path(), splitPaths)
-	html3Route := len(rel) > 0 && rel[0] == "html3"
+
 	switch {
-	case html3Route:
+	case IsHTML3(c.Path()):
 		if err := html3.Error(err, c); err != nil {
-			panic(err) // TODO: logger?
+			log.DPanic("Custom HTML3 response handler broke: %s", err)
 		}
 		return
 	default:
-		if err := X(err, c); err != nil {
-			panic(err) // TODO: logger?
+		code := http.StatusInternalServerError
+		if he, ok := err.(*echo.HTTPError); ok {
+			code = he.Code
+		}
+		errorPage := fmt.Sprintf("%d.html", code)
+		if err := c.File(errorPage); err != nil {
+			// fallback to a string error if templates break
+			if err1 := StringErr(err, c); err1 != nil {
+				log.DPanic("Custom response handler broke: %s", err1)
+			}
 		}
 		return
 	}
-
-	//c.Logger().Error(err)
-
-	// errorPage := fmt.Sprintf("%d.html", code)
-	// if err := c.File(errorPage); err != nil {
-	// 	c.Logger().Error(err)
-	// }
 }
 
-func X(err error, c echo.Context) error {
+func StringErr(err error, c echo.Context) error {
 	code, msg := http.StatusInternalServerError, "internal server error"
 	if he, ok := err.(*echo.HTTPError); ok {
 		code = he.Code
 		msg = fmt.Sprint(he.Message)
 	}
-	return c.String(code, fmt.Sprintf("%d - %s!", code, msg))
+	return c.String(code, fmt.Sprintf("%d - %s", code, msg))
+}
+
+func IsHTML3(path string) bool {
+	splitPaths := func(r rune) bool {
+		return r == '/'
+	}
+	rel := strings.FieldsFunc(path, splitPaths)
+	return len(rel) > 0 && rel[0] == "html3"
 }
