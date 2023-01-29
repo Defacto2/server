c7be14622af574dc08c06cb750ea4e09dcf6be1f
Ben Garrett <code.by.ben@gmail.com>
2023-01-30 09:08:03 +1100

    Improved the db connection methods to allow custom values.

--- pkg/postgres/postgres.go
+++ pkg/postgres/postgres.go
@@ -7,6 +7,7 @@ package postgres
 import (
 	"database/sql"
 	"fmt"
+	"net/url"
 	"strconv"
 	"strings"
 )
@@ -15,20 +16,66 @@ const (
 	Name = "postgres"
 )
 
+// Connection details of the PostgreSQL database connection.
+type Connection struct {
+	Protocol  string // Protocol scheme of the PostgreSQL database. Defaults to postgres.
+	User      string // User is the database user used to connect to the database.
+	Password  string // Password is the password for the database user.
+	HostName  string // HostName is the host name of the server. Defaults to localhost.
+	HostPort  int    // HostPort is the port number the server is listening on. Defaults to 5432.
+	Database  string // Database is the database name.
+	NoSSLMode bool   // NoSSLMode connects to the database using an insecure, plain text connecction using the sslmode=disable param.
+}
+
+// Open opens a PostgreSQL database connection.
+func (c Connection) Open() (*sql.DB, error) {
+	conn, err := sql.Open(Name, c.URL())
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
+
+// URL returns a url used as a PostgreSQL database connection.
+func (c Connection) URL() string {
+	// "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable"
+	if c.Protocol == "" {
+		c.Protocol = Name
+	}
+	if c.HostName == "" {
+		c.HostName = "localhost"
+	}
+	if c.HostPort < 1 {
+		c.HostPort = 5432
+	}
+	var usr *url.Userinfo
+	if c.User != "" && c.Password != "" {
+		usr = url.UserPassword(c.User, c.Password)
+	} else if c.User != "" {
+		usr = url.User(c.User)
+	}
+	dns := url.URL{
+		Scheme: c.Protocol,
+		User:   usr,
+		Host:   fmt.Sprintf("%s:%d", c.HostName, c.HostPort),
+		Path:   c.Database,
+	}
+	if c.NoSSLMode {
+		q := dns.Query()
+		q.Set("sslmode", "disable")
+		dns.RawQuery = q.Encode()
+	}
+	return dns.String()
+}
+
 // ConnectDB connects to the PostgreSQL database.
 func ConnectDB() (*sql.DB, error) {
-	const (
-		protocol = "postgres"
-		user     = "root"
-		password = "example"
-		hostname = "localhost"
-		hostport = 5432
-		database = "defacto2-ps"
-		options  = "sslmode=disable"
-	)
-	dsn := fmt.Sprintf("%s://%s:%s@%s:%d/%s?%s",
-		protocol, user, password, hostname, hostport, database, options)
-	conn, err := sql.Open(Name, dsn)
+	dsn := Connection{
+		User:     "root",
+		Password: "example",
+		Database: "defacto2-ps",
+	}
+	conn, err := sql.Open(Name, dsn.URL())
 	if err != nil {
 		return nil, err
 	}
---
+++ pkg/postgres/postgres_test.go
@@ -0,0 +1,46 @@
+package postgres_test
+
+import (
+	"testing"
+
+	"github.com/Defacto2/server/pkg/postgres"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConnection_URL(t *testing.T) {
+	type fields struct {
+		Protocol  string
+		User      string
+		Password  string
+		HostName  string
+		HostPort  int
+		Database  string
+		NoSSLMode bool
+	}
+	tests := []struct {
+		fields fields
+		want   string
+	}{
+		{fields{}, "postgres://localhost:5432"},
+		{fields{Protocol: "dbproto"}, "dbproto://localhost:5432"},
+		{fields{User: "dbuser"}, "postgres://dbuser@localhost:5432"},
+		{fields{User: "dbuser", Password: "xyz"}, "postgres://dbuser:xyz@localhost:5432"},
+		{fields{HostName: "myserver"}, "postgres://myserver:5432"},
+		{fields{HostName: "myserver", HostPort: 5678}, "postgres://myserver:5678"},
+		{fields{HostName: "myserver", HostPort: 5678, NoSSLMode: true}, "postgres://myserver:5678?sslmode=disable"},
+		{fields{Database: "my_db", HostName: "myserver", HostPort: 5678, NoSSLMode: true}, "postgres://myserver:5678/my_db?sslmode=disable"},
+	}
+	for _, tt := range tests {
+		c := postgres.Connection{
+			Protocol:  tt.fields.Protocol,
+			User:      tt.fields.User,
+			Password:  tt.fields.Password,
+			HostName:  tt.fields.HostName,
+			HostPort:  tt.fields.HostPort,
+			Database:  tt.fields.Database,
+			NoSSLMode: tt.fields.NoSSLMode,
+		}
+		assert := assert.New(t)
+		assert.Equal(tt.want, c.URL(), "url string does not match")
+	}
+}
