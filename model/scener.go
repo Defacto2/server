0c23c116982215846058bc84b4062654648a95c4
Ben <code.by.ben@gmail.com>
2023-08-17 10:28:13 +1000

    placeholder.

---
+++ model/scener.go
@@ -0,0 +1,17 @@
+package model
+
+// SELECT *
+// FROM "files"
+// WHERE "credit_text" ILIKE '%Absolute Zero%' OR "credit_program" ILIKE '%Absolute Zero%' OR "credit_illustration" ILIKE '%Absolute Zero%' OR "credit_audio" ILIKE '%Absolute Zero%'
+// LIMIT 50
+
+// SELECT *
+// FROM "files"
+// WHERE "credit_text" ILIKE '%ab%,' OR
+// "credit_program" ILIKE '%ab%,' OR
+// "credit_illustration" ILIKE '%ab%,' OR
+// "credit_audio" ILIKE '%ab%,'
+// LIMIT 50
+
+// TODO matchText matchProg etc (name string) string
+// match ben ben, ,ben, ,ben
