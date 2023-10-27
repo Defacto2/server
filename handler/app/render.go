862fc62613e14f5ba782fe53a221a5a87d88c650
Ben Garrett <code.by.ben@gmail.com>
2023-10-27 15:05:07 +1100

    improve history page and text.

--- handler/app/render.go
+++ handler/app/render.go
@@ -214,9 +214,7 @@ func History(z *zap.SugaredLogger, c echo.Context) error {
 	if z == nil {
 		return InternalErr(z, c, name, ErrZap)
 	}
-	const lead = "Defacto2 started in late February or early March of 1996," +
-		" as an electronic magazine that wrote about The Scene subculture." +
-		" In the past, alternative iterations of the name have included" +
+	const lead = "In the past, alternative iterations of the name have included" +
 		" De Facto, DF, DeFacto, Defacto II, Defacto 2, and the defacto2.com domain."
 	const h1 = "The history of the brand"
 	data := empty()
