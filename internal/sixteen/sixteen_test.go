0f18ff4f489004f09adcdbc6b3baac6d4f31cd31
Ben Garrett <code.by.ben@gmail.com>
2024-01-30 08:44:21 +1100

    Add unit test for sixteen.Find() function

---
+++ internal/sixteen/sixteen_test.go
@@ -0,0 +1,16 @@
+package sixteen_test
+
+import (
+	"testing"
+
+	"github.com/Defacto2/server/internal/sixteen"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFind(t *testing.T) {
+	t.Parallel()
+	tag := sixteen.Find("defacto2")
+	assert.Equal(t, tag, sixteen.GroupTag("group/defacto 2"))
+	tag = sixteen.Find("notfound")
+	assert.Equal(t, tag, sixteen.GroupTag(""))
+}
