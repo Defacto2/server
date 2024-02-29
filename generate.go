50dba76b43b54e695fdd90ffa231918432c237ff
Ben Garrett <code.by.ben@gmail.com>
2024-02-29 12:13:02 +1100

    More header comments.

--- generate.go
+++ generate.go
@@ -1,3 +1,10 @@
+// SQLBoiler command to generate Go code from a PostgreSQL database schema.
+// https://github.com/volatiletech/sqlboiler
+//
+// It requires an active PostgreSQL server to be running.
+// To rebuild run this command in the terminal:
+//
+// $ go generate
 package main
 
 //go:generate sqlboiler --config "sqlboiler.toml" --wipe --add-soft-deletes psql
