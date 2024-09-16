17129dd84dbeaeae5195fe521e18e410d206dac5
Code by Ben <code.by.ben@gmail.com>
2024-09-17 09:53:37 +1000

    fixed wasm loading issues.

--- handler/app/asset.go
+++ handler/app/asset.go
@@ -38,7 +38,7 @@ func Hrefs() Paths {
 		Bootstrap5JS:    "/js/bootstrap.bundle.min.js",
 		BootstrapIcons:  "/svg/bootstrap-icons.svg",
 		DosboxJS:        "/js/wdosbox.js",
-		DosboxWasm:      "/js/wdosbox.wasm.js",
+		DosboxWasm:      "/js/wdosbox.wasm",
 		EditArtifact:    "/js/editor-artifact.min.js",
 		EditAssets:      "/js/editor-assets.min.js",
 		EditForApproval: "/js/editor-forapproval.min.js",
